8be93b5d58f99a9511d7deef49cffde62e651c35
DBond1980 <t0503814174@gmail.com>
2023-02-27 15:35:03 +0200

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+///cbcvbcxbcvbxcvb
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
